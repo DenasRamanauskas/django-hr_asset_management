f86c96dd4ef6005ce21e6013c043ffb97219bd9e
DenasRamanauskas <denas.ramanauskas@gmail.com>
2023-03-13 12:15:33 +0200

    added sign-up form

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -10,3 +10,28 @@ def home(request):
     handles requests to the home page.
     '''
     return render(request, 'core/home.html')
+
+
+# handles employer signup requests
+def employer_signup(request):
+    if request.method == 'POST':
+        form = EmployerSignupForm(request.POST)
+        if form.is_valid():
+            form.save()  # add employer to db
+
+            # authenticate and login employer
+            email = form.cleaned_data.get('email')
+            password1 = form.cleaned_data.get('password1')
+            user = authenticate(request, email=email, password=password1)
+            if user is not None:
+                login(request, user)
+                return redirect('core:employer_dashboard')
+    else:
+        form = EmployerSignupForm()
+
+    return render(request, 'core/employer_signup.html', {'form': form})
+
+
+# the employer dashboard
+def employer_dashboard(request):
+    return render(request, 'core/employer_dashboard.html')
