8e191742447d3dc0160dd2a0b44ba01db92b3500
DenasRamanauskas <denas.ramanauskas@gmail.com>
2023-03-13 14:26:02 +0200

    few changes

--- mysite/core/forms.py
+++ mysite/core/forms.py
@@ -10,7 +10,7 @@ class EmployerSignupForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'phone_number', 'password1', 'password2')
 
     @transaction.atomic
     def save(self):
